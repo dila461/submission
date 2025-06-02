4c2c86d0711b046de03f56978a36142ed97d2290
fadhilahazzahra <fadhilahazzahra461@gmail.com>
2025-06-03 04:35:55 +0800

    ubah halaman pertama dan about

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const menuOpenButton = document.querySelector('#menu-open-button');
-const menucloseButton = document.querySelector('#menu-close-button');
+const menuCloseButton = document.querySelector('#menu-close-button');
 
 menuOpenButton.addEventListener("click", () => {
     document.body.classList.toggle("show-mobile-menu");
