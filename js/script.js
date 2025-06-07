d52c96766d2e75245d6709d34c077890013d8cfd
fadhilahazzahra <fadhilahazzahra461@gmail.com>
2025-06-08 01:30:59 +0800

    s

--- js/script.js
+++ js/script.js
@@ -5,7 +5,5 @@ menuOpenButton.addEventListener("click", () => {
     document.body.classList.toggle("show-mobile-menu");
 });
 
-const menuCloseButton = document.querySelector('#menu-close-button');
-
 menuCloseButton.addEventListener("click", () =>
 menuOpenButton.click());
