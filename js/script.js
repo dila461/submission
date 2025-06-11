758bde5c3c7e9a375cd62e59aea6db2e24395f71
ismifadilah <ismifdlhh12.gmail.com>
2025-06-12 01:29:06 +0800

    modified and deleted

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
-const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.nav-menu');
+document.addEventListener('DOMContentLoaded', function() {
+  const hamburger = document.querySelector('.hamburger');
+  const navMenu = document.querySelector('.nav-menu');
+  const navOverlay = document.querySelector('.nav-overlay');
+  const closeMenu = document.querySelector('.close-menu');
 
-hamburger.addEventListener('click', () => {
-  navMenu.classList.toggle('active');
-  hamburger.classList.toggle('toggle');
+  function closeSidebar() {
+    navMenu.classList.remove('active');
+    hamburger.classList.remove('open');
+    navOverlay.classList.remove('active');
+  }
+
+  hamburger.addEventListener('click', function() {
+    navMenu.classList.add('active');
+    hamburger.classList.add('open');
+    navOverlay.classList.add('active');
+  });
+
+  closeMenu.addEventListener('click', closeSidebar);
+  navOverlay.addEventListener('click', closeSidebar);
 });
